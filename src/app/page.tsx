256d821770f243386b4cbc6af54d71f1d1eaa96c
hariskamran <hariskamran1999.hk@gmail.com>
2025-10-22 18:30:51 +0500

    Make layout responsive by adjusting `main` width and height styles for different screen sizes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import PaymentViewer from "@/components/PaymentViewer";
 export default function Home(): ReactElement {
   return (
     <div className="flex h-screen items-center justify-center font-sans bg-zinc-200">
-      <main className="w-[420px] h-auto bg-zinc-50 p-4">
+      <main className="w-full h-screen md:h-auto md:w-[420px]  bg-zinc-50 p-4">
         <Header />
         <PaymentViewer />
         <Footer />
