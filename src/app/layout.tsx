46785758def2d2d24e720a429fc03b396931d2b9
hariskamran <hariskamran1999.hk@gmail.com>
2025-10-22 16:42:09 +0500

    Update `children` type in RootLayout from `ReactElement` to `ReactNode` to allow broader compatibility

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import type { ReactElement } from "react";
+import type { ReactElement, ReactNode } from "react";
 
 import "@/app/globals.css";
 
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: ReactElement;
+  children: ReactNode;
 }>): ReactElement {
   return (
     <html lang="en">
